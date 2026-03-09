956e50dd9961395ded14db786858530ead4159ed
Mohammed Sadiq <mohammed.sadiq@imparta.com>
2026-03-09 20:54:33 +0400

    no message

--- tests/Plugin.Maui.CustomTabbedPage.Tests/CustomTabPageDefaultsTests.cs
+++ tests/Plugin.Maui.CustomTabbedPage.Tests/CustomTabPageDefaultsTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Plugin.Maui.CustomTabbedPage.Controls;
+using Xunit;
 
 namespace Plugin.Maui.CustomTabbedPage.Tests;
 
--- tests/Plugin.Maui.CustomTabbedPage.Tests/iOSTabBarModeTests.cs
+++ tests/Plugin.Maui.CustomTabbedPage.Tests/iOSTabBarModeTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Plugin.Maui.CustomTabbedPage;
+using Xunit;
 
 namespace Plugin.Maui.CustomTabbedPage.Tests;
 
